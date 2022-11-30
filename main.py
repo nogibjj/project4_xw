d3ae73c074bb779ce0bcdc316e716668b95b5576
Yitong Wang <90431341+xxwwwww@users.noreply.github.com>
2022-11-30 04:45:58 +0000

    fastapi

--- main.py
+++ main.py
@@ -1,56 +1,51 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI() 
 
 
-# class Book():
-#     name: str
-#     writer: str
-#     description: str
+class Book(BaseModel):
+    name: str
+    writer: str
+    price: float
+    description: str
 
-
-# store_booklist = []
+booklist = []
 
 @app.get("/") 
 async def root():
-    return {"message": "Hello Project 4 This is a book library. Add/Read/Delete/Update the book you like"}
-
-# # create
-# @app.post("/add/")
-# async def create_book(book: Book):
-#     store_booklist.append(book)
-#     return book
+    return {"message": "Hello, welcome to the electronic library. You can Search/Read/Add the book here"}
 
 
-# # read
-# @app.get("/getall/", response_model=List[Book])
-# async def get_all_books():
-#     return store_booklist
+# See booklist
+@app.get("/booklist/", response_model=List[Book])
+async def get_booklist():
+    return booklist
 
+# Add
+@app.post("/add/")
+async def add_book(book: Book):
+    booklist.append(book)
+    return book
 
-# # delete
-# @app.delete("/delete/{name}")
-# async def delete_book(name: str):
-#     for i, x in enumerate(store_booklist):
-#         if x.name == name:
-#             obj = store_booklist[i]
-#             store_booklist.pop(i)
-#             return obj
+# Delete
+@app.delete("/delete/{name}/{writer}")
+async def delete_book(name: str, writer: str):
+    for i, x in enumerate(booklist):
+        if x.name == name and x.writer == writer:
+            obj = booklist[i]
+            booklist.pop(i)
+            return obj
 
-#     raise HTTPException(status_code=404, detail="ook Not Found")
-
-
-# # update
-# @app.put("/update/{name}")
-# async def update_book(name: str, book: Book):
-#     for i, x in enumerate(store_booklist):
-#         if x.name == name:
-#             store_booklist[i] = book
-#             return store_booklist[i]
-
-#     raise HTTPException(status_code=404, detail="Book Not Found")
-
+# Search
+@app.get("/search/{keyword}")
+async def search_book(keyword: str):
+    for i, x in enumerate(booklist):
+        if x.name == keyword or x.writer == keyword or x.description == keyword:
+            obj = booklist[i]
+            return obj
 
 
 if __name__ == "__main__":
